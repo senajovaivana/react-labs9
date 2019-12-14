c9b0e5ce922f3024df4df037d7bfe79175f6845e
Ivana Senajova <senajovaivana@gmail.com>
2019-12-14 22:19:06 +0100

    Lab10Ex4

--- src/components/PageEmployeesList.jsx
+++ src/components/PageEmployeesList.jsx
@@ -1,47 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-import { employeesLoaded } from '../redux/actions'
+import { employeesLoaded } from "../redux/actions";
 
-const EmployeeLine = ({ employee }) => <div>{employee.name} ({employee.age} yrs old): {employee.company}</div>
+const EmployeeLine = ({ employee }) => (
+  <div>
+    {employee.name} ({employee.age} yrs old): {employee.company}
+  </div>
+);
 
 class PageEmployeesList extends React.Component {
-
   constructor(props) {
     super(props);
 
-    this.state = { 
-      isLoading: false,
-    }
+    this.state = {
+      isLoading: false
+    };
   }
 
   componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch('http://localhost:3004/employees')
-    .then((data) => data.json())
-    // Without Redux
-    // .then((employees) => this.setState({ employees, isLoading: false }));
-    // With Redux
-    .then((employees) => {
-      this.props.employeesLoaded(employees);
-      this.setState({ isLoading: false });
-    });
+    if (this.props.employees === undefined) {
+      this.setState({isLoading: true});
+      fetch("http://localhost:3004/employees")
+          .then(data => data.json())
+          // Without Redux
+          // .then((employees) => this.setState({ employees, isLoading: false }));
+          // With Redux
+          .then(employees => {
+            this.props.employeesLoaded(employees);
+            this.setState({isLoading: false});
+          });
+    }
   }
 
   render() {
     const { isLoading } = this.state;
     const { employees } = this.props;
 
-    if(isLoading) {
-      return <p>Loading ...</p>
+    if (isLoading) {
+      return <p>Loading ...</p>;
     }
-    
+
     return (
       <div>
         <h1>Employees List:</h1>
-        {employees && employees.map((employee => <EmployeeLine key={employee._id} employee={employee} />))}
+        {employees &&
+          employees.map(employee => (
+            <EmployeeLine key={employee._id} employee={employee} />
+          ))}
         <Link to="/new">
+          {/* eslint-disable-next-line react/button-has-type */}
           <button>Create employee</button>
         </Link>
       </div>
@@ -49,17 +58,14 @@ class PageEmployeesList extends React.Component {
   }
 }
 
-const mapStateToProps = (state /*, ownProps*/) => {
+const mapStateToProps = (state /* , ownProps */) => {
   return {
     employees: state.employees
-  }
-}
+  };
+};
 
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   employeesLoaded: employees => dispatch(employeesLoaded(employees))
-})
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PageEmployeesList)
+export default connect(mapStateToProps, mapDispatchToProps)(PageEmployeesList);
